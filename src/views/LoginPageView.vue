1412fa4f2201ebf23a76f6ea0f93d10aea16f8cf
leychenkoMaxim <max242009@ukr.net>
2024-02-12 09:09:35 +0200

    change some function

--- src/views/LoginPageView.vue
+++ src/views/LoginPageView.vue
@@ -165,9 +165,9 @@ export default {
 		  toggleModal(){
 				this.modalActive = !this.modalActive;
 			},
-        async onLoginWithGoogle() {
+         onLoginWithGoogle() {
             try {
-                await this.loginWithGoogle()
+                this.loginWithGoogle()
                 this.$router.push({
                     name: 'dishes',
                 })
